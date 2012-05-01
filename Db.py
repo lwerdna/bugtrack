1111cd62fda9adf60829a6188fcf8e2199d4dd13
James Thompson <jamesdavidthompson@gmail.com>
2012-04-30 22:23:50 -0700

    Initial check-in. several things not working.

--- Db.py
+++ Db.py
@@ -7,33 +7,101 @@
 # initially, I'll make a model dbText, which uses simple text files for
 # storage....
 
+import sqlite3
+import string
+from ConfigParser import SafeConfigParser
+
 class Db(object):
+
+    #--------------------------------------------------------------------------
+    # database maintenance
+    #--------------------------------------------------------------------------
+    SCHEMA_GAMES = [
+            ['time',            'INTEGER'],  # Timestamp in epoch seconds
+            ['teamAwhite',      'TEXT'],     # Winner - White player
+            ['teamAwhiteRating','INTEGER'],  # Winner - White player rating
+            ['teamAblack',      'TEXT'],     # Winner - Black player
+            ['teamAblackRating','INTEGER'],  # Winner - Black player rating
+            ['teamBwhite',      'TEXT'],     # Loser  - White player
+            ['teamBwhiteRating','INTEGER'],  # Loser  - White player rating
+            ['teamBblack',      'TEXT'],     # Loser  - Black player
+            ['teamBblackRating','INTEGER']]  # Loser  - Black player rating
+    SCHEMA_PLAYERS = [
+            ['name',  'TEXT'],     # Name
+            ['rating','INTEGER'],  # Rating
+            ['rd',    'INTEGER']]  # Rd
+
+    # Database configuration and initialization ------------------------------
+    def createDatabase(self):
+        print '\tDropping old tables'
+        cmd = 'DROP TABLE IF EXISTS ' + self.config.get('Database','table_games') + ';'
+        self.c.execute(cmd)
+        cmd = 'DROP TABLE IF EXISTS ' + self.config.get('Database','table_players') + ';'
+        self.c.execute(cmd)
+        print '\tCreating new tables'
+
+        # Games
+        cmd = 'CREATE TABLE ' + self.config.get('Database','table_games') + ' (id INTEGER PRIMARY KEY, '
+        for field in self.SCHEMA_GAMES:
+            cmd += field[0] + ' ' + field[1] + ', '
+        cmd = cmd[:-2]
+        cmd += ');'
+        self.c.execute(cmd)
+
+        # Players
+        cmd = 'CREATE TABLE ' + self.config.get('Database','table_players') + ' (id INTEGER PRIMARY KEY, '
+        for field in self.SCHEMA_PLAYERS:
+            cmd += field[0] + ' ' + field[1] + ', '
+        cmd = cmd[:-2]
+        cmd += ');'
+        self.c.execute(cmd)
+
+        self.conn.commit()
+
     #--------------------------------------------------------------------------
     # general info
     #--------------------------------------------------------------------------
     # return list of players
     def getPlayerList(self):
-        pass
+        self.c.execute('SELECT name from ' + self.config.get('Database','table_players') + ';')
+        return [ row for row in self.c.fetchall() ]
+
+    def createNewPlayer(self, name, rating):
+        cmd = 'INSERT into ' + self.config.get('Database','table_players') + ' (name,rating,rd) VALUES (\'' + name + '\',' + str(rating) + ',350);'
+        self.c.execute(cmd)
+        self.conn.commit()
+
     #--------------------------------------------------------------------------
     # get player stats
     #--------------------------------------------------------------------------
     # get the player's rating
     def getPlayerRating(self, name):
-        pass
+        self.c.execute('SELECT rating from ' + self.config.get('Database','table_players') + ' WHERE (name = \'' + name + '\');')
+        return [ row for row in self.c.fetchall() ][0][0]
+
     # get the player's RD
     def getPlayerRD(self, name):
-        pass
+        self.c.execute('SELECT rd from ' + self.config.get('Database','table_players') + ' WHERE (name = \'' + name + '\');')
+        return [ row for row in self.c.fetchall() ][0][0]
+
     # return a list [rating, RD]
     def getPlayerStats(self, name):
-        pass
+        self.c.execute('SELECT rating,rd from ' + self.config.get('Database','table_players') + ' WHERE (name = \'' + name + '\');')
+        return [ row for row in self.c.fetchall() ][0]
 
     #--------------------------------------------------------------------------
     # set player stats
     #--------------------------------------------------------------------------
     def setPlayerRating(self, name, r):
-        pass
+        cmd = 'UPDATE ' + self.config.get('Database','table_players') + ' set rating = ' + str(r) + ' WHERE (name = \'' + name + '\');'
+        self.c.execute(cmd)
+        self.conn.commit()
+
     def setPlayerRD(self, name, rd):
-        pass
+        cmd = 'UPDATE ' + self.config.get('Database','table_players') + ' set rd = ' + str(rd) + ' WHERE (name = \'' + name + '\');'
+        self.c.execute(cmd)
+        self.conn.commit()
+
     def setPlayerStats(self, name, listStats):
         pass
 
@@ -51,11 +119,26 @@ class Db(object):
     #
     # (change this comment if the db schema changes please)
     def getGames(self, since):
-        pass
+        self.c.execute('SELECT * from ' + self.config.get('Database','table_games') + ' WHERE (time > ' + str(since) + ');'
+        return [ row for row in self.c.fetchall() ]
 
     # retrieve all games that had player involved in it
     def getGamesByPlayer(self, name, since):
         pass
+
+    def recordGame(self, t, teamAWhite, tawRating, teamABlack, tabRating, \
+               teamBWhite, tbwRating, teamBBlack, tbbRating):
+        cmd = 'INSERT INTO ' + self.config.get('Database','table_games') + '('
+        for field in self.SCHEMA_GAMES:
+             cmd += field[0] + ','
+        cmd = cmd[:-1]
+        cmd += ') VALUES (\''
+        for value in (t,teamAWhite, tawRating, teamABlack, tabRating, teamBWhite, tbwRating, teamBBlack, tbbRating):
+            cmd += str(value) + '\',\''
+        cmd = cmd[:-3]
+        cmd += '\');'
+        self.c.execute(cmd)
+        self.conn.commit()
 
     #--------------------------------------------------------------------------
     # setup/testing stuff
@@ -65,6 +148,15 @@ class Db(object):
     #
     # for example. DbText creates players.dat and games.dat
     #
-    def init(self):
+    def __init__(self):
+        # Read configuration file
+        self.config = SafeConfigParser()
+        self.config.read('config.ini')
+
+        # Connect to database
+        print 'Connecting to database [' + self.config.get('Database','filename') + ']...'
+        self.conn = sqlite3.connect(self.config.get('Database','filename'))
+        self.c = self.conn.cursor()
+
         pass
 
