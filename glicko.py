60d5035365dc6d0d8269b8a2c38fcd56d53a7898
andrewl <andrew@home>
2012-05-25 23:11:47 -0400

    fixed idiot error in glicko calculation, rater module deleted
    stupidly I used math.exp() thinking it was "exponentiate", but
    it's with default base e - needed math.pow() instead

--- glicko.py
+++ glicko.py
@@ -19,20 +19,23 @@ c = 0.023582175925925927
 # t is seconds since last time he played a game
 # w is 1 for win, 0 for loss
 #
-def glicko(ratings, rds, t, w):
+def calcRatingRdPlayer(ratings, rds, t, w):
     # RD adjustment due to time
     rd = rds[0]
     rd = math.sqrt(rd*rd + c*t)
+    # keep within range [30, 350]
     rd = min(rd, 350)
+    rd = max(rd, 30)
 
     # attenuating factor
     p = .0000025180996504909944  # 3*math.log(10)*math.log(10) / (math.pi*math.pi * 800 * 800)
     f = 1/math.sqrt(1 + p*(rds[1]*rds[1] + rds[2]*rds[2] + rds[3]*rds[3]))
+    # f should be [0, 1] ...large RD's result in small f's
 
     # average ratings
-    r1 = (ratings[0]+ratings[1])/2
-    r2 = (ratings[2]+ratings[3])/2
-    E = 1/(1 + math.exp(-1*(r1-r2)*f/400))
+    r1 = (ratings[0]+ratings[1])/2.0
+    r2 = (ratings[2]+ratings[3])/2.0
+    E = 1/(1 + math.pow(10, -1*(r1-r2)*f/400))
 
     # begin-pre
     q = 0.0028782313662425573 # math.log(10)/800
@@ -47,6 +50,49 @@ def glicko(ratings, rds, t, w):
     # done
     return [rating, rd]
 
-def glickoDelta(ratings, rds, t, w):
+def calcRatingRdDeltaPlayer(ratings, rds, t, w):
     [newRating, newRD] = glicko(ratings, rds, t, w)
     return [newRating - ratings[0], newRD - rds[0]]
+
+# if a1 (with partner a2, opponents b1,b2) plays, return his <+win>,<-loss> adjustment
+# returns [winDelta, loseDelta]
+def calcRatingWinLossDeltaPlayer(ratings, rds, ts):
+    [winDelta, winRD] = calcRatingRDDelta_player(ratings, rds, int(time.time()) - ts[0], 1)
+    [loseDelta, loseRD] = glicko.glickoDelta(ratings, rds, int(time.time()) - ts[0], 0)
+    #print "%d,%d" % (winDelta, loseDelta)
+    return [winDelta, loseDelta]
+
+# suppose a game is played with winners a1,a2 and losers a3,a4
+# given [a1,a2,a3,a4], return the new ratings/rd's in this form:
+# [[a1_RATING, a1_RD], [a2_RATING, a2_RD], [b1_RATING, b1_RD], [b2_RATING, b2_RD]]
+#
+def calcGameScores(ratings, rds, tds):
+    # calculate new scores
+
+    # calculate for a1 (first winner, white)
+    stats1 = calcRatingRdPlayer(ratings, rds, tds[0], 1)
+    #print "calling calcRatingRdPlayer(", ratings, ", ", rds, ", ", tdelta, ", 1)<br>\n"
+    #print "a1 new stats: ", stats1, '<br>\n'
+
+    # calculate for a2 (first winner, black)
+    ratings = [ratings[1], ratings[0], ratings[2], ratings[3]]
+    rds = [rds[1], rds[0], rds[2], rds[3]]
+    stats2 = calcRatingRdPlayer(ratings, rds, tds[1], 1)
+    #print "calling calcRatingRdPlayer(", ratings, ", ", rds, ", ", tdelta, ", 1)<br>\n"
+    #print "a2 new stats: ", stats2, '<br>\n'
+
+    # calculate for b1 (first loser, black)
+    ratings = [ratings[2], ratings[3], ratings[0], ratings[1]]
+    rds = [rds[2], rds[3], rds[0], rds[1]]
+    stats3 = calcRatingRdPlayer(ratings, rds, tds[2], 0)
+    #print "calling calcRatingRdPlayer(", ratings, ", ", rds, ", ", tdelta, ", 0)<br>\n"
+    #print "b1 new stats: ", stats3, '<br>\n'
+
+    ratings = [ratings[1], ratings[0], ratings[2], ratings[3]]
+    rds = [rds[1], rds[0], rds[2], rds[3]]
+    stats4 = calcRatingRdPlayer(ratings, rds, tds[3], 0)
+    #print "calling calcRatingRdPlayer(", ratings, ", ", rds, ", ", tdelta, ", 0)<br>\n"
+    #print "b2 new stats: ", stats4, '<br>\n'
+
+    return [stats1, stats2, stats3, stats4]
+
