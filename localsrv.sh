fb288f7f9749b606ddaa99a3ed0336b787cea9bf
Scott Bissett <scott.bissett@gmail.com>
2012-05-29 01:12:29 -0400

    modified localsrv.sh to use cgi-bin & CGIHTTPServer when serving on Macs added Python server.py script

--- localsrv.sh
+++ localsrv.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-thttpd -p 2048 -c "*.py"
+if [[ $OSTYPE != darwin* ]]; then  # for non-Mac systems...
+	thttpd -p 2048 -c "*.py"
+else
+	rm cgi-bin/*
+	for f in *.py; do ln $f cgi-bin/$f; done
+	python server.py
+	rm cgi-bin/*
+fi
